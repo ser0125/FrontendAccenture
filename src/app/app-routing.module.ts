a53a1ee54a07c2f879b31b26d1decbcfa8c2fbf0
ser0125 <sergio.llanos.garcia@gmail.com>
2018-10-06 17:42:44 -0500

    Se añade la vista del registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { RegisterComponent } from './components/register/register.component';
 import { LogInComponent } from './components/log-in/log-in.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
@@ -5,8 +6,9 @@ import { NgModule } from '@angular/core';
 
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/logIn', pathMatch: 'full' },
-    { path: 'logIn', component: LogInComponent}
+    { path: '', redirectTo: '/ingreso', pathMatch: 'full' },
+    { path: 'ingreso', component: LogInComponent},
+    { path: 'registro', component: RegisterComponent}
   ];
 
   @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogInComponent
+    LogInComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
